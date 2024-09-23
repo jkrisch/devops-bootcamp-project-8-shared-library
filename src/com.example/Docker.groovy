554380fa29baecb4bed433fdd6cb75a112347be9
jonas <jonas.krisch@gmail.com>
2024-09-23 16:09:00 +0200

    adjusting Docker.groovy

--- src/com.example/Docker.groovy
+++ src/com.example/Docker.groovy
@@ -1,5 +1,4 @@
 #!/user/bin/env groovy
-
 package com.example
 
 class Docker implements Serializable {
@@ -10,15 +9,11 @@ class Docker implements Serializable {
     }
 
     def buildImage(String username, String password, String repo, String appname){
-
+        script.echo 'building docker image'
+        script.sh """
+            echo ${password} | docker login -u ${username} --password-stdin
+            docker build -t  ${repo}/${appname}:jma-${VERSION} .
+            docker push ${repo}/${appname}:jma-${VERSION}
+        """
     }
-}
-
-def call(username, password, repo, appname, version){
-    script.echo 'building docker image'
-    script.sh """
-        echo ${password} | docker login -u ${username} --password-stdin
-        docker build -t  ${repo}/${appname}:jma-${VERSION} .
-        docker push ${repo}/${appname}:jma-${VERSION}
-    """
 }
